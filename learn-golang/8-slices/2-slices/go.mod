3dabfffbaa0af7ffc9dadc092f92b61c5cbfa1e0
Silvestre Espinoza <silverpeltisthebest@gmail.com>
2025-07-25 16:53:30 -0500

    learn-golang - Chapter 8 - Lesson 2

---
+++ learn-golang/8-slices/2-slices/go.mod
@@ -0,0 +1,3 @@
+module learn-golang/8-slices/2-slices
+
+go 1.24.5
---
+++ learn-golang/8-slices/2-slices/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "errors"
+
+const (
+	planFree = "free"
+	planPro  = "pro"
+)
+
+func getMessageWithRetriesForPlan(plan string, messages [3]string) ([]string, error) {
+	switch plan {
+	case planFree:
+		return messages[:2], nil
+	case planPro:
+		return messages[:], nil
+	default:
+		return nil, errors.New("unsupported plan")
+	}
+}
